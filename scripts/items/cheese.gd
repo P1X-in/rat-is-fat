91d9ea736ded9653bdd18b2fc5cf35579315dd2c
CzlowiekImadlo <quak@tlen.pl>
2015-07-25 14:53:00 +0200

    Added room doors, fixed cheese script

--- scripts/items/cheese.gd
+++ scripts/items/cheese.gd
@@ -1,8 +1,15 @@
 extends "res://scripts/items/abstract_item.gd"
 
+var body
+
 func _init(bag).(bag):
     self.avatar = preload("res://scenes/items/cheese.xscn").instance()
+    self.body = self.avatar.get_node('body')
+    self.randomize_frame()
 
 func die():
     self.despawn()
 
+func randomize_frame():
+    randomize()
+    self.body.set_frame(randi() % self.body.get_hframes())
