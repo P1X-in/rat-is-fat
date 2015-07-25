6c52e31e677691eaf012a7c8b5979f1ea114f4ca
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-07-25 18:25:47 +0200

    press start to join

--- scripts/hud/player_panel.gd
+++ scripts/hud/player_panel.gd
@@ -8,6 +8,18 @@ func _init(bag, panel_node):
 
 func show():
     self.panel.show()
+    self.joined()
 
 func hide():
     self.panel.hide()
+    self.dead()
+
+func joined():
+    self.panel.get_node('dead').hide()
+    self.panel.get_node('join').hide()
+    self.panel.get_node('stats').show()
+
+func dead():
+    self.panel.get_node('dead').show()
+    self.panel.get_node('join').hide()
+    self.panel.get_node('stats').hide()
