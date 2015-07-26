e431511d21d82ff0e9b95c66dbb9f5e8b6e62839
CzlowiekImadlo <quak@tlen.pl>
2015-07-26 12:14:08 +0200

    New Shia Prime scene, reduced shake

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,14 +4,12 @@ var camera
 
 var zoom
 
-var shake_timer
 var shakes = 0
-export var shakes_max = 5
-export var shake_time = 0.1
-export var shake_boundary = 5
+var shakes_max = 3
+var shake_time = 0.1
+var shake_boundary = 5
+
 var shake_initial_position
-var temp_delta = 0
-var pos
 
 func _init_bag(bag):
     self.bag = bag
@@ -19,23 +17,25 @@ func _init_bag(bag):
     self.zoom = self.camera.get_zoom()
 
 func shake():
-    shakes = 0
-    shake_initial_position = self.camera.get_camera_pos()
+    self.shakes = 0
+    self.shake_initial_position = self.camera.get_camera_pos()
     self.do_single_shake()
 
 func do_single_shake():
-    if shakes < shakes_max:
-        var distance_x = randi() % shake_boundary
-        var distance_y = randi() % shake_boundary
+    var pos
+    var distance_x
+    var distance_y
+    if self.shakes < self.shakes_max:
+        distance_x = randi() % self.shake_boundary
+        distance_y = randi() % self.shake_boundary
         if randf() <= 0.5:
             distance_x = -distance_x
         if randf() <= 0.5:
             distance_y = -distance_y
 
-        pos = shake_initial_position + Vector2(distance_x, distance_y)
+        pos = self.shake_initial_position + Vector2(distance_x, distance_y)
         self.camera.set_offset(pos)
-        shakes += 1
-        self.bag.timers.set_timeout(0.15, self, "do_single_shake")
+        self.shakes = self.shakes + 1
+        self.bag.timers.set_timeout(self.shake_time, self, "do_single_shake")
     else:
-        pos = shake_initial_position
         self.camera.set_offset(shake_initial_position)
--- scripts/enemies/shia_prime.gd
+++ scripts/enemies/shia_prime.gd
@@ -1,7 +1,7 @@
 extends "res://scripts/enemies/abstract_boss.gd"
 
 func _init(bag).(bag):
-    self.avatar = preload("res://scenes/enemies/shia.xscn").instance()
+    self.avatar = preload("res://scenes/enemies/shia_prime.xscn").instance()
     self.body_part_head = self.avatar.get_node('body')
     self.body_part_body = self.avatar.get_node('body')
     self.body_part_footer = self.avatar.get_node('body')
