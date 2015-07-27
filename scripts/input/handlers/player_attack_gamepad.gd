864cc0b268b8fbd70807949193fab1f522d7614f
CzlowiekImadlo <quak@tlen.pl>
2015-07-27 21:59:15 +0200

    Final gamepad tweaks

--- scripts/input/handlers/player_attack_gamepad.gd
+++ scripts/input/handlers/player_attack_gamepad.gd
@@ -3,17 +3,19 @@ extends "res://scripts/input/handlers/gamepad_handler.gd"
 var bag
 var player
 
+var reverse
+
 func _init(bag, player):
     self.bag = bag
     self.player = player
     self.type = InputEvent.JOYSTICK_MOTION
     self.axis = Globals.get("platform_input/xbox_right_trigger")
+    self.reverse = Globals.get("platform_input/xbox_reverse_trigger")
 
 func handle(event):
     if self.bag.game_state.game_in_progress && self.player.is_playing && self.player.is_alive:
-        if event.value > 0.5:
-            if not self.player.is_attack_on_cooldown:
-                self.player.attack()
+        if (not self.reverse && event.value > 0.5) || (self.reverse && event.value < -0.25):
             self.player.is_attacking = true
+            self.player.attack()
         else:
             self.player.is_attacking = false
